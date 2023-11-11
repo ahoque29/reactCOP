168653ba1d1d6d018a8bc93e7ca009e4601d68e9
ahoque29 <a.hoque29@hotmail.com>
2023-11-11 00:22:34 +0000

    Add alert data

---
+++ alert-inbox/src/alerts.ts
@@ -0,0 +1,16 @@
+﻿export const alertData = [
+    {
+        id: 1,
+        type: "Low disk space",
+        source: "localhost",
+        status: "Active",
+        lastUpdated: "15:18"
+    },
+    {
+        id: 2,
+        type: "Blocking process",
+        source: "localhost",
+        status: "Active",
+        lastUpdated: "15:18"
+    }
+];
